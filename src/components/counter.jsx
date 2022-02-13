f3bd0d314582595cf88be26b072f707e159e7993
Dagm Niko <dagmniko79@gmail.com>
2022-02-13 11:32:33 +0300

    Add changeType functionality

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -7,10 +7,13 @@ class Counter extends Component {
   render() {
     return (
       <Fragment>
-        <span>{this.state.count}</span>
+        <span>{changeType()}</span>
         <button>Increment</button>
       </Fragment>
     );
+    const changeType = () => {
+      this.count.state === 0 ? <h1>Zero</h1> : this.count.state;
+    }
   }
 }
 
